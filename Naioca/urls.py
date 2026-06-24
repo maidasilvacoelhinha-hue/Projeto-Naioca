d0349a071f986d62acbbc8c443dc4fc956cbd9a6
EVELYN MAIDA DA SILVA COELHO <20261016270002@ifrn.local>
2026-06-23 21:36:46 -0300

    deixando bonito

--- Naioca/urls.py
+++ Naioca/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "nome_do_app"
 
 urlpatterns = [
-    path("", views.telainicial, name="telainicial"),
+    path("telainicial/", views.telainicial, name="telainicial"),
     path("paginadelogin/", views.paginadelogin, name="paginadelogin"),
     path("sobre/", views.sobre, name="sobre"),
 ]
--- Naioca/views.py
+++ Naioca/views.py
@@ -10,3 +10,10 @@ def paginadelogin(request):
 def sobre(request):
     return render(request,"Naioca/sobre.html")
 
+def tela_inicial(request):
+    itens = [
+        {"nome": "Bolo de chocolate", "descricao": "Bolo de chocolate delicioso e fofinho."},
+        {"nome": "Item 2", "descricao": "Descrição do Item 2"},
+        {"nome": "Item 3", "descricao": "Descrição do Item 3"},
+    ]
+    return render(request, "Naioca/tela_inicial.html", {"itens": itens})
